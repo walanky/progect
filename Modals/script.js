f8479eb013c0245901bfa87bda4a732e51dda649
walanky <quasar02@icloud.com>
2020-06-19 07:30:46 +0300

    s

--- Modals/script.js
+++ Modals/script.js
@@ -46,8 +46,25 @@ navigator.sayswho = (function () {
    return M.join(' ');
 })();
 
+function browser() {
+   var ua = navigator.userAgent;
 
-alert(navigator.sayswho);
+   if (ua.search(/MSIE/) > 0) return 'Internet Explorer';
+   if (ua.search(/Firefox/) > 0) return 'Firefox';
+   if (ua.search(/Opera/) > 0) return 'Opera';
+   if (ua.search(/Chrome/) > 0) return 'Google Chrome';
+   if (ua.search(/Safari/) > 0) return 'Safari';
+   if (ua.search(/Konqueror/) > 0) return 'Konqueror';
+   if (ua.search(/Iceweasel/) > 0) return 'Debian Iceweasel';
+   if (ua.search(/SeaMonkey/) > 0) return 'SeaMonkey';
+
+   // Браузеров очень много, все вписывать смысле нет, Gecko почти везде встречается
+   if (ua.search(/Gecko/) > 0) return 'Gecko';
+
+   // а может это вообще поисковый робот
+   return 'adsad';
+}
+alert(browser);
 // ======================
 
 // Наше модальное окно 
